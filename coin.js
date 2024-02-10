d92a9e2023b622eb948d412eb1b3217e34cd8420
albrthuynh <128447235+albrthuynh@users.noreply.github.com>
2024-02-10 09:38:33 -0600

    Add files via upload

--- coin.js
+++ coin.js
@@ -1,26 +1,6 @@
 var numCoins = 0;
 
-function increaseCoin() {
-    numCoins++;
-    updateCounter();
-}
-
-function updateCounter() {
-    var countNum = document.getElementById('coin-counter');
-
-    if(!countNum) {
-        countNum = document.createElement('div');
-        countNum.id = 'coin-counter';
-        document.body.appendChild(countNum);
-    }
-
-    countNum.innerText = numCoins;
-
-    counterElement.style.position = 'absolute';
-    counterElement.style.top = '5%';
-    counterElement.style.right = '45%';
-    counterElement.style.fontSize = '20px';
-    counterElement.style.color = 'white';
-}
-
-updateCounter();
+function coinInc() {
+    numCoins += 1;
+    document.getElementById("coincounter").innerHTML = numCoins;
+  };
